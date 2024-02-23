b29ea3837b2c0d563de18bd1bb29d19a708f00ff
Veeno <sebastien.cloiseau@gmail.com>
2024-02-23 23:46:53 +0100

    feature: creat own exercice from zero

---
+++ server/schema/exercice.js
@@ -0,0 +1,115 @@
+const graphql = require("graphql");
+const _ = require("lodash");
+
+const {
+  GraphQLID,
+  GraphQLObjectType,
+  GraphQLString,
+  GraphQLList,
+  GraphQLSchema,
+  GraphQLInt,
+  GraphQLFloat,
+} = graphql;
+
+var assets = [
+  {
+    name: "Wormhole",
+    symbol: "W",
+    id: "1",
+    blockchain: ["Ethereum", "Avalanche", "Cardano", "Solana"],
+    price: 1,
+  },
+  {
+    name: "Ethereum",
+    symbol: "ETH",
+    id: "2",
+    blockchain: ["Ethereum"],
+    price: 2800,
+  },
+  {
+    name: "The Graph",
+    symbol: "GRT",
+    id: "3",
+    blockchain: ["Ethereum"],
+    price: 0.24,
+  },
+  {
+    name: "Chainlink",
+    symbol: "LINK",
+    id: "4",
+    blockchain: ["Ethereum", "Solana"],
+    price: 18,
+  },
+  {
+    name: "Avalanche",
+    symbol: "AVAX",
+    id: "5",
+    blockchain: ["Avalanche", "Cardano", "Ethereum"],
+    price: 45,
+  },
+  {
+    name: "Cardano",
+    symbol: "ADA",
+    id: "6",
+    blockchain: ["Cardano"],
+    price: 0.6,
+  },
+];
+
+var blockchains = [
+  { name: "Ethereum", id: "1" },
+  { name: "Avalanche", id: "2" },
+  { name: "Cardano", id: "3" },
+  { name: "Solana", id: "4" },
+];
+
+const BlockchainType = new GraphQLObjectType({
+  name: "blockchain",
+  fields: () => ({
+    name: { type: GraphQLString },
+    id: { type: GraphQLID },
+    assets: {
+      type: GraphQLList(AssetsType),
+      resolve(parent, args) {
+        return _.filter(assets, (asset) =>
+          asset.blockchain.includes(parent.name)
+        );
+      },
+    },
+  }),
+});
+
+const AssetsType = new GraphQLObjectType({
+  name: "Asset",
+  fields: () => ({
+    name: { type: GraphQLString },
+    id: { type: GraphQLID },
+    symbol: { type: GraphQLString },
+    blockchain: { type: GraphQLList(GraphQLString) },
+    price: { type: GraphQLFloat },
+  }),
+});
+
+const RootQuery = new GraphQLObjectType({
+  name: "RootQueryType",
+  fields: {
+    blockchain: {
+      type: BlockchainType,
+      args: { id: { type: GraphQLID } },
+      resolve(parent, args) {
+        return _.find(blockchains, { id: args.id });
+      },
+    },
+    asset: {
+      type: AssetsType,
+      args: { name: { type: GraphQLString } },
+      resolve(parent, args) {
+        return _.find(assets, { name: args.name });
+      },
+    },
+  },
+});
+
+module.exports = new GraphQLSchema({
+  query: RootQuery,
+});
